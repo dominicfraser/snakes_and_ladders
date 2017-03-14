68db16ca93702249747e71d1deb40530e5e7706f
Dominic <mrdjfraser@gmail.com>
2017-03-14 22:34:25 +0000

    spelling mistake fixed in class name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,7 @@ require ("minitest/rg")
 Dir["../*.rb"].each {|file| require file }
 #ONLY WORKS IF IN CORRECT STARTING DIR IN TERMINAL
 
-class TestPlatey < MiniTest::Test 
+class TestPlayer < MiniTest::Test 
 
   def setup
     @player1 = Player.new("Jake")
