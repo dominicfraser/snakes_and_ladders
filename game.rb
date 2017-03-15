6db6561c34d4b29cd4c1e690af73d9fb8bcb654c
Dominic <mrdjfraser@gmail.com>
2017-03-15 14:15:26 +0000

    added greet and end methods

--- game.rb
+++ game.rb
@@ -8,13 +8,51 @@ class GameEngine
     @die = die
   end
 
+  def greet()
+    puts "Hi!"
+  end
 
+  def end()
+    puts "Bye!"
+  end
 
-
-
-  
-
-
-  
 
 end
+
+
+# greet // end 
+# populate players
+# turn 
+# win
+# switch players
+
+
+# turn:
+
+# WHILE game status = not won 
+# player = whoever
+# position = whatever
+# -role dice
+# -move
+#   -if position = winning 
+#     then return game status = won
+#   elsif  position = ladder.start hash
+#     then move again
+#   elsif position = snake.start hash
+#     then move again
+#   else 
+#     position = position
+#   end
+# - change player 
+
+ 
+
+      
+
+
+
+
+
+
+  
+
