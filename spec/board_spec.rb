86c2d0d7980b3fe41b47b5eaee2245b6abc8c3b2
Dominic <mrdjfraser@gmail.com>
2017-03-15 11:23:06 +0000

    test board has squares passed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,12 @@ Dir["../*.rb"].each {|file| require file }
 class TestBoard < MiniTest::Test 
 
   def setup
-    @board1 = Board.new(50)
+    @board1 = Board.new(100)
   end
+
+  def test_board_has_squares
+    assert_equal(100, @board1.no_squares)
+  end
+
 
 end
